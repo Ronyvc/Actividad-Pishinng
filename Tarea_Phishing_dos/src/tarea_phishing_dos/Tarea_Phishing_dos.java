1cd794094f4182c09c405e590e01c354c110590b
Ronyvc <148804095+Ronyvc@users.noreply.github.com>
2023-12-22 23:21:31 -0500

    Add files via upload

---
+++ Tarea_Phishing_dos/src/tarea_phishing_dos/Tarea_Phishing_dos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_phishing_dos;
+
+public class Tarea_Phishing_dos {
+
+   
+    public static void main(String[] args) {
+          String[] keywords = {
+            "Importante", "Acceso a cuenta", "Premio", "Gana dinero rápido", "Ganador afortunado",
+            "Gratis total", "Bloqueo cuenta", "Vencimiento de cuenta", "Verificación necesaria", "Actualizar datos",
+            "Confirmación de identidad", "Contraseña segura", "Verificación cuenta", "Actualización urgente requerida", "Oferta especial exclusiva",
+            "Problemas de seguridad detectados", "Suspensión temporal de cuenta", "Promoción única y especial", "Actualiza información",
+            "Problemas inicio de sesión", "Obtén tu premio", "Restablecer la cuenta", "Problema inicio de sesión", "Oferta limitada en el tiempo",
+            "Recupera el acceso", "Advertencia de seguridad importante", "Confirma tu identidad", "Oferta promocional increíble",
+            "Realiza la actualización ahora", "Cierre temporal de cuenta"
+        };
+          
+    }
+    
+}
